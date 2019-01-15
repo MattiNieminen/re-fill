3ffcd6596223774ce8ee8530eb2d76c55f312edf
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2019-01-15 11:41:00 +0200

    Update all dependency versions

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.7.1"
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.854" :scope "provided"]
-                 [re-frame "0.10.1" :scope "provided"]
-                 [reagent "0.8.0-alpha1" :scope "provided"]
-                 [bidi "2.1.2"]
-                 [kibu/pushy "0.3.8"]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
+                 [re-frame "0.10.6" :scope "provided"]
+                 [reagent "0.8.1" :scope "provided"]
+                 [bidi "2.1.5"]
+                 [lifecheq/pushy "0.3.9"]
                  [cljs-uuid "0.0.4"]]
-  :plugins [[lein-figwheel "0.5.12"]
+  :plugins [[lein-figwheel "0.5.18"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
   :cljsbuild {:builds
               [{:id "dev"
@@ -24,7 +24,7 @@
                            :source-map-timestamp true
                            :preloads [devtools.preload]}}]}
   :figwheel {:css-dirs ["example-resources/public/css"]}
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]]
                    :clean-targets ^{:protect false} ["dev-build/public/js/"
                                                      :target-path]
                    :resource-paths ["dev-build" "example-resources"]}})
